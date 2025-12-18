f45fbf7f95fe39de437cfa09e264ce6f9ef3ec68
Tushar Biswas <tusharbiswas0786@Mac.bbrouter>
2025-12-19 04:04:28 +0530

    Finding length of string and also finding count of $ in string

---
+++ Input_name_and_legth_of_name.py
@@ -0,0 +1,9 @@
+Name = input("Enter you name : ")
+
+print(len(Name))
+
+#Find occurance of dollar in string
+
+str = "today i got 2$ and ramesh having 5$ and both we have 7$"
+
+print(str.count("$"))
