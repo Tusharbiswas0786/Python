2d35bc58876e499f7d831f1254ce1b69ce8f1eb2
Tushar Biswas <tusharbiswas0786@Mac.bbrouter>
2025-12-20 00:54:38 +0530

    Conditional statements with example

--- If_Elif_else.py
+++ If_Elif_else.py
@@ -1,10 +1,24 @@
-Number = int(input("Enter a number : "))
+# Number = int(input("Enter a number : "))
 
-if(Number >= 10):
-    print("number is greater than 10")
-elif(Number <= 10):
-    print("Number is Less than 10")
-else:
-    print("Invalid number")
+# if(Number >= 10):
+#     print("number is greater than 10")
+# elif(Number <= 10):
+#     print("Number is Less than 10")
+# else:
+#     print("Invalid number")
+
+#GRADE OF STUDENTS
+
+Marks = int(input("Enter marks for student : "))
+
+if(Marks >= 90):
+    print("Grade of the student is A")
+elif(Marks >= 80 and Marks < 90):
+    print("grade of the student is B")
+elif(Marks >= 70 and Marks < 80):
+    print("grade of the student is C")
+elif(Marks < 70):
+    print("grade of the student is D")
+
 
 
