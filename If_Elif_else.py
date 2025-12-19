456b0b5b61690230459d661669722518d1b6a9a2
Tushar Biswas <tusharbiswas0786@Mac.bbrouter>
2025-12-20 00:44:23 +0530

    Conditional statements

---
+++ If_Elif_else.py
@@ -0,0 +1,10 @@
+Number = int(input("Enter a number : "))
+
+if(Number >= 10):
+    print("number is greater than 10")
+elif(Number <= 10):
+    print("Number is Less than 10")
+else:
+    print("Invalid number")
+
+
