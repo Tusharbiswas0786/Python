810b094bb919da976d18cdf708a9a9f0db875291
Tushar Biswas <tusharbiswas0786@Tushars-MacBook-Air.local>
2025-12-21 15:37:37 +0530

    Tuple practice question

---
+++ Tuple_practice.py
@@ -0,0 +1,13 @@
+#WAP to count the no. of students with the "A" grade in following tuple.
+tup = ("C", "D", "A", "A", "B", "B", "A" )
+
+print(tup.count("A"))
+
+
+#WAP to store the above values in list and sort them from "A" to "D"
+
+list = ["C", "D", "A", "A", "B", "B", "A"]
+
+list.sort()
+
+print(list)
